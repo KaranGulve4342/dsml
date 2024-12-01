64ec8155df323def05db8923c7c24d3c8909a308
KaranGulve4342 <karangulve4342@gmail.com>
2024-12-02 00:21:51 +0530

    all added

---
+++ Problem12.py
@@ -0,0 +1,19 @@
+# 12.Use Iris flower dataset and perform following :
+# 1. Create a box plot for each feature in the dataset.
+# 2. Identify and discuss distributions and identify outliers from them.
+
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('C:/Users/DELL/DSML_PRACTICAL/Datasets/IRIS.csv')
+
+numeric_features = ["sepal_length","sepal_width","petal_length","petal_width"]
+
+for feature in numeric_features:
+    sns.boxplot(x=data[feature],color="red")
+    plt.title(f"Box plot of {feature}")
+    plt.xlabel(feature)
+    plt.show()
+
---
+++ Problem15.py
@@ -0,0 +1,3 @@
+# 15. Use the dataset 'titanic'. The dataset contains 891 rows and contains
+# information about the passengers who boarded the unfortunate Titanic
+# ship. Use the Seaborn library to see if we can find any patterns in the data.
